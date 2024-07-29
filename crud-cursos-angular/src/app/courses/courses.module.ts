45ee15b7af19e4f8974a9817ae6ced42e97fb57a
Deborah Gomes Souza <deborah270691@gmail.com>
2024-07-28 21:08:07 -0300

    primeiro commit do projeto

---
+++ crud-cursos-angular/src/app/courses/courses.module.ts
@@ -0,0 +1,18 @@
+import { CoursesComponent } from './courses/courses.component';
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoursesRoutingModule } from './courses-routing.module';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
+@NgModule({
+  declarations: [
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule
+  ]
+})
+export class CoursesModule { }
